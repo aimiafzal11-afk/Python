538e067c07009b26396f828c1028510cdb62f994
Aiman Afzal <aimiafzal.11@gmail.com>
2026-07-24 22:13:08 +0500

    Add files via upload

---
+++ QsOfSet&Dict.py
@@ -0,0 +1,21 @@
+#Store 2 meanings of table and 1 meaning of cat in dictionary
+dict = {
+    "table" : ["a piece of furniture", "list of facts and figures"],
+    "cat" : "a small animal"
+    }
+print(dict)
+
+#Each lang needs separate class then how many classes are needed?
+set = {"python", "java", "C++", "python", "javascript", "java", "python", "java", "C++", "C"}
+print(len(set))
+
+#Input marks of 3 subjects and store them in dictionary
+marks = {}
+sub1 = input("Enter marks of English: ")
+marks["English"] = sub1
+sub2 = input("Enter marks of Maths: ")
+marks["Maths"] = sub2
+sub3 = input("Enter marks of Science: ")
+marks["Science"] = sub3
+
+print(marks)
